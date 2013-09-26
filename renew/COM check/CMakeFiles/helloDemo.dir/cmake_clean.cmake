f58d0b2bec9b8365a853ac92763104d725ac67e4
saka <saka@saka-Vostro-200.(none)>
2013-09-26 14:14:20 +0900

    COM Jacobian

---
+++ renew/COM check/CMakeFiles/helloDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/helloDemo.dir/main.cpp.o"
+  "helloDemo.pdb"
+  "helloDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/helloDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
